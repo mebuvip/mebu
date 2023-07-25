fa9694f4a0324d339e9de25f88dc6cf5acf969ef
mebuvip <contact@mebu.vip>
2023-07-25 10:45:51 +0200

    Adding auditors suggestions

--- contract.sol
+++ contract.sol
@@ -1,28 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity = 0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @custom:security-contact contact@mebu.vip
-contract MemeBuddha is ERC20, ERC20Burnable, Pausable, Ownable {
+contract MemeBuddha is ERC20, ERC20Burnable, Ownable {
     address public charityWallet;
     address public communityWallet;
     mapping (address => bool) public liquidityPools;
 
-    constructor() ERC20("Meme Buddha", "MEBU") {
-        _mint(msg.sender, 108000000000 * 10 ** decimals());
-    }
-
-    function setCharityWallet(address _charityWallet) public onlyOwner {
+    constructor(address _charityWallet, address _communityWallet) ERC20("Meme Buddha", "MEBU") {
         require(_charityWallet != address(0), "Invalid charity wallet address");
-        charityWallet = _charityWallet;
-    }
-
-    function setCommunityWallet(address _communityWallet) public onlyOwner {
         require(_communityWallet != address(0), "Invalid community wallet address");
+
+        _mint(msg.sender, 108000000000 * 10 ** decimals());
+        charityWallet = _charityWallet;
         communityWallet = _communityWallet;
     }
 
@@ -36,22 +30,10 @@ contract MemeBuddha is ERC20, ERC20Burnable, Pausable, Ownable {
         liquidityPools[_liquidityPool] = false;
     }
 
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
     function _beforeTokenTransfer(address from, address to, uint256 amount)
-    internal
-    override
+        internal
+        override
     {
-        if (from != owner()) {
-            require(!paused(), "ERC20Pausable: token transfer while paused");
-        }
-
         if (liquidityPools[to] && from != charityWallet && from != communityWallet) {
             uint256 fee = amount * 6 / 100;
             uint256 distributeAmount = fee / 3;
